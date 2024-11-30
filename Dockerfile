ff55fd3990a5dfce0bdbf8cc1776b497507f4a81
Aylon Ben Dvora <70889286+I-ABD-I@users.noreply.github.com>
2024-11-30 15:23:03 +0200

    Add Dockerfile and enable HTTPS for the server; update .gitignore and remove unused fib worker

---
+++ Dockerfile
@@ -0,0 +1,45 @@
+FROM node:22-alpine AS base
+
+FROM base AS deps
+# setup workdir
+WORKDIR /app
+
+# install deps
+COPY package.json package-lock.json ./
+RUN npm ci --only=production
+
+FROM base AS builder
+# setup workdir
+WORKDIR /app
+
+# copy deps & code
+COPY --from=deps /app/node_modules ./node_modules
+COPY . .
+
+# build
+RUN npm run build
+
+FROM base AS runner
+
+# setup workdir
+WORKDIR /app
+
+# declare env
+ENV NODE_ENV=production
+
+# add users
+RUN addgroup --system --gid 1001 nodejs
+RUN adduser --system --uid 1001 expressjs
+
+# copy project files
+COPY --from=builder /app/dist ./
+COPY --from=builder /app/node_modules ./node_modules
+COPY cert.pem key.pem ./
+
+USER expressjs
+
+EXPOSE 3001
+
+ENV PORT=3001
+
+CMD ["node", "index.js"]
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from "express";
 import { Worker, WorkerOptions } from "worker_threads";
+import https, { ServerOptions } from "https";
+import { readFileSync } from "fs";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware to parse JSON
 app.use(express.json());
@@ -30,34 +32,55 @@ const runWorker = (
     return { worker, workerPromise };
 };
 
-app.post("/fib", async (req: Request<{}, {}, { number: number }>, res: Response) => {
-    const { number } = req.body;
-    if (!number) {
-        res.status(400).send("Invalid Input, number is required");
-        return;
-    }
-
-    const { worker, workerPromise } = runWorker("./src/fib.cjs", { workerData: number });
-
-    const terminateWorker = () => {
-        if (req.destroyed) {
-            worker.terminate();
-            console.log("Request closed, worker terminated.");
+app.post(
+    "/fib",
+    async (req: Request<{}, {}, { number: number }>, res: Response) => {
+        const { number } = req.body;
+        if (!number) {
+            res.status(400).send("Invalid Input, number is required");
+            return;
         }
-    };
 
-    req.socket.on("close", terminateWorker);
-    try {
-        const resp = await workerPromise;
-        res.send(resp.toString());
-    } catch {
-        res.sendStatus(500);
-    } finally {
-        req.socket.off("close", terminateWorker);
+        const { worker, workerPromise } = runWorker(
+            `const { parentPort, workerData } = require("worker_threads");
+
+            const fib = (index) => {
+                if (index == 0) return 0;
+                if (index == 1 || index == 2) return 1;
+
+                return fib(index - 1) + fib(index - 2);
+            };
+
+            parentPort?.postMessage(fib(workerData));`,
+            {
+                workerData: number,
+                eval: true,
+            }
+        );
+
+        const terminateWorker = () => {
+            if (req.destroyed) {
+                worker.terminate();
+                console.log("Request closed, worker terminated.");
+            }
+        };
+
+        req.socket.on("close", terminateWorker);
+        try {
+            const resp = await workerPromise;
+            res.send(resp.toString());
+        } catch {
+            res.sendStatus(500);
+        } finally {
+            req.socket.off("close", terminateWorker);
+        }
     }
-});
+);
 
 // Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
-});
+const serverOpts: ServerOptions = {
+    cert: readFileSync("./cert.pem"),
+    key: readFileSync("./key.pem"),
+    passphrase: "test",
+};
+https.createServer(serverOpts, app).listen(PORT);
