467f297cea5068ed6c02ff6f2c1d5c03aaac90fb
Aylon Ben Dvora <70889286+I-ABD-I@users.noreply.github.com>
2025-04-05 00:09:32 +0300

    Update Dockerfile and index.ts to use new certificate names; enhance response formatting

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import express, { Request, Response } from "express";
-import { Worker, WorkerOptions } from "worker_threads";
-import https, { ServerOptions } from "https";
-import { readFileSync } from "fs";
+import express, {Request, Response} from "express";
+import {Worker, WorkerOptions} from "worker_threads";
+import {readFileSync} from "node:fs";
+import https, {ServerOptions} from "https";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,11 +11,11 @@ app.use(express.json());
 
 // Basic route
 app.get("/", (req: Request, res: Response) => {
-    res.send("How Did You Get Here?!?");
+    res.send("How Did You Get Here?!?\n");
 });
 
 app.get("/ping", (req: Request, res: Response) => {
-    res.send("pong!");
+    res.send("pong!\n");
 });
 
 const runWorker = (
@@ -29,19 +29,19 @@ const runWorker = (
         worker.on("message", resolve);
     });
 
-    return { worker, workerPromise };
+    return {worker, workerPromise};
 };
 
 app.post(
     "/fib",
     async (req: Request<{}, {}, { number: number }>, res: Response) => {
-        const { number } = req.body;
+        const {number} = req.body;
         if (!number) {
-            res.status(400).send("Invalid Input, number is required");
+            res.status(400).send("Invalid Input, number is required\n");
             return;
         }
 
-        const { worker, workerPromise } = runWorker(
+        const {worker, workerPromise} = runWorker(
             `const { parentPort, workerData } = require("worker_threads");
 
             const fib = (index) => {
@@ -68,7 +68,7 @@ app.post(
         req.socket.on("close", terminateWorker);
         try {
             const resp = await workerPromise;
-            res.send(resp.toString());
+            res.send(resp.toString() + '\n');
         } catch {
             res.sendStatus(500);
         } finally {
@@ -79,10 +79,21 @@ app.post(
 
 // Start the server
 const serverOpts: ServerOptions = {
-    cert: readFileSync("./cert.pem"),
-    key: readFileSync("./key.pem"),
-    passphrase: "test",
+    cert: readFileSync("./heavy-cert.pem"),
+    key: readFileSync("./heavy-key.pem"),
 };
-https.createServer(serverOpts, app).listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+const server = https.createServer(serverOpts, app).listen(PORT, () => {
+    console.log(`HTTPS Server is listening on port ${PORT}`);
 });
+
+// http.createServer(app).listen(PORT, () => {
+//     console.log(`Server is listening on port ${PORT}`);
+// });
+
+process.on("SIGTERM", () => {
+    server.close()
+})
+
+process.on("SIGINT", () => {
+    server.close()
+})
